7e6a3701f355565d61a55c810514565b956b67ea
Marcos <marcos.c.junior2@gmail.com>
2024-03-03 14:23:23 -0300

    Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1,"Books");
-		
-		System.out.println(dep);
+		Seller sel = new Seller(1,"Bob","Bob@",new Date(),3000.0,dep);
+		System.out.println(sel);
 	}
 
 }
